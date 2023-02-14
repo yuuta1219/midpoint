fe0aeb1cb73c18c4e7a3054f8d04e598f77ec8c2
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-02-14 20:45:24 +0900

    環境構築

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js',
+    './app/frontend/entrypoints/*.{js,ts,jsx,tsx}',
+    './app/frontend/src/*.{js,ts,jsx,tsx}',
+  ],
+  plugins: [require("daisyui")],
+  daisyui: { 
+    themes: ["winter"],
+  },
+}
