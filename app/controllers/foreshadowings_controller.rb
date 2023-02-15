107f65cf0a80169ab6a8c9dc9edeb156a0fb0cdf
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-02-15 17:13:42 +0900

    ページ作成の完了

--- app/controllers/foreshadowings_controller.rb
+++ app/controllers/foreshadowings_controller.rb
@@ -1,70 +1,26 @@
 class ForeshadowingsController < ApplicationController
-  before_action :set_foreshadowing, only: %i[ show edit update destroy ]
-
-  # GET /foreshadowings or /foreshadowings.json
   def index
-    @foreshadowings = Foreshadowing.all
+    @plot = Plot.find(params[:plot_id])
+    @foreshadowings = @plot.foreshadowings.order(created_at: :asc)
   end
 
-  # GET /foreshadowings/1 or /foreshadowings/1.json
-  def show
-  end
-
-  # GET /foreshadowings/new
   def new
     @foreshadowing = Foreshadowing.new
   end
-
-  # GET /foreshadowings/1/edit
-  def edit
-  end
-
-  # POST /foreshadowings or /foreshadowings.json
+  
   def create
+    @plot = Plot.find(params[:plot_id])
     @foreshadowing = Foreshadowing.new(foreshadowing_params)
-
-    respond_to do |format|
-      if @foreshadowing.save
-        format.html { redirect_to foreshadowing_url(@foreshadowing), notice: "Foreshadowing was successfully created." }
-        format.json { render :show, status: :created, location: @foreshadowing }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @foreshadowing.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /foreshadowings/1 or /foreshadowings/1.json
-  def update
-    respond_to do |format|
-      if @foreshadowing.update(foreshadowing_params)
-        format.html { redirect_to foreshadowing_url(@foreshadowing), notice: "Foreshadowing was successfully updated." }
-        format.json { render :show, status: :ok, location: @foreshadowing }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @foreshadowing.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /foreshadowings/1 or /foreshadowings/1.json
-  def destroy
-    @foreshadowing.destroy
-
-    respond_to do |format|
-      format.html { redirect_to foreshadowings_url, notice: "Foreshadowing was successfully destroyed." }
-      format.json { head :no_content }
+    if @foreshadowing.save
+      redirect_to plot_foreshadowings_path(@plot), success: "作成しました！"
+    else
+      redirect_to plot_foreshadowings_path(@plot), danger: "作成できませんでした"
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_foreshadowing
-      @foreshadowing = Foreshadowing.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def foreshadowing_params
-      params.fetch(:foreshadowing, {})
-    end
+  def foreshadowing_params
+    params.permit(:name, :body).merge(plot_id: params[:plot_id])
+  end
 end
