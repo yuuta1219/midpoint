f1539df5cf7474448ea563bdcb89968368722df2
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-03-03 18:35:49 +0900

    レイアウト修正

--- db/migrate/20230212114635_create_eighteen_lines.rb
+++ db/migrate/20230212114635_create_eighteen_lines.rb
@@ -1,7 +1,9 @@
 class CreateEighteenLines < ActiveRecord::Migration[7.0]
   def change
     create_table :eighteen_lines do |t|
+      t.integer :number
       t.string :name
+      t.string :explanation
       t.string :body
       t.references :plot, foreign_key: true
 
