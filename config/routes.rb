0ed409d63f4567efcd1b159172597088b77d5120
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-03-10 17:32:40 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -14,11 +14,10 @@ Rails.application.routes.draw do
   get 'new_guest', to: 'users#new_guest'
   
   resources :users, only: %i[new create destroy]
-  resources :plots, shallow: true do
-    resources :eighteen_lines
-    resources :cards
-    resources :characters
-    resources :foreshadowings
-    resources :timelines
+  resources :plots,  shallow: true do
+    resources :eighteen_lines, only: %i[create update destroy]
+    resources :cards, only: %i[index edit new create update destroy]
+    resources :characters, only: %i[index new create update destroy]
+    resources :foreshadowings, only: %i[index create update destroy]
   end
 end
