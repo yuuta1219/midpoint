3849121328a85c1be882ea47f0072424a56984a4
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-03-10 16:44:06 +0900

    TOPページ仮完成

--- app/frontend/src/Explanation.tsx
+++ app/frontend/src/Explanation.tsx
@@ -4,30 +4,44 @@ import Fade from 'react-reveal/Fade';
 import card from './card.png';
 import cardSecond from './cardSecond.png';
 import cardThird from './cardThird.png';
+import topImage from './topImage.jpg';
+import topImage2 from './topImage2.png';
 
 const Explanation = () => {
   return (
     <>
-    <div className="grid grid-cols-3">
+    <div className="grid grid-cols-3 border-b border-neutral-900">
     </div>
     <div className="grid grid-cols-2 sm:grid-cols-4 mt-20 pb-10 bg-neutral-50 drop-shadow-2xl">
       <div className="col-span-2 sm:col-span-4">
         <div className="m-2 my-4 text-neutral border-b border-neutral-700 text-4xl ">使い方</div>
       </div>
-      <div className="col-span-2 text-neutral text-xl xl:text-3xl mb-5 mx-3">
+      <div className="col-span-2 mx-2 mt-10 drop-shadow-2xl border border-neutral-500">
+      <img src={topImage} alt="topImage" className="w-full"/>
+      </div>
+      <div className="col-span-2 mt-10 text-neutral text-xl xl:text-3xl mb-5 mx-3">
       <Fade bottom cascade> 
-        <div className="border-b border-neutral-700">カードタブ</div>
+        <div className="border-b border-neutral-700">アカウント作成</div>
         <br></br>
-        グラフとカードを使用した視覚的にわかりやすいプロット管理が可能に。
+        アカウントを作成することでサービスの利用が可能になります。
         <br></br>
-        トグルを利用することで、画面上に表示したいものを自由に選択できます。
+        お試しにはゲストログインをご利用ください。
       </Fade>
       </div>
-      <div className="col-span-2 mx-2 drop-shadow-2xl">
-      
+      <div className="col-span-2 mt-10 text-neutral text-xl xl:text-3xl mb-5 mx-3">
+      <Fade bottom cascade> 
+        <div className="border-b border-neutral-700">プロット作成</div>
+        <br></br>
+        流れに沿って進めるだけで本格的なストーリープロットを作成することができます。
+        <br></br>
+        
+      </Fade>
+      </div>
+      <div className="col-span-2 mt-10 mx-2 drop-shadow-2xl border border-neutral-500">
+        <img src={topImage2} alt="topImage2" className="w-full"/>
       </div>
       <Fade bottom cascade>
-      <div className="col-span-2 mx-2 drop-shadow-2xl">
+      <div className="col-span-2 mt-10 mx-2 drop-shadow-2xl border border-neutral-500">
         <div className="carousel w-full ">
           <div id="slide1" className="carousel-item relative w-full">
             <img src={card} alt="card" className="w-full"/>
@@ -53,7 +67,7 @@ const Explanation = () => {
         </div> 
       </div>
       </Fade>
-      <div className="col-span-2 text-neutral text-xl xl:text-3xl my-5 mx-3">
+      <div className="col-span-2 mt-10 text-neutral text-xl xl:text-3xl my-5 mx-3">
       <Fade bottom cascade>
         <div className="border-b border-neutral-700">カードタブ</div>
         <br></br>
@@ -62,9 +76,9 @@ const Explanation = () => {
         トグルを利用することで、画面上に表示したいものを自由に選択できます。
         </Fade>
       </div>
-      <div className="col-span-2 mx-2 drop-shadow-2xl">
+      <div className="col-span-2 mt-10 mx-2 drop-shadow-2xl">
       </div>
-      <div className="col-span-2 mx-2 drop-shadow-2xl">
+      <div className="col-span-2 mt-10 mx-2 drop-shadow-2xl">
       </div>
     </div>
     </>
