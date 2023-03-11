ce0a768a3667f1dfb46e0e05316e7eb20ca4b0e5
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-03-11 09:56:03 +0900

    テキストサイズ調整

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,7 +1,9 @@
 class UserSessionsController < ApplicationController
   skip_before_action :require_login, only: %i[new create]
 
-  def new;end
+  def new
+    @color = "normal"
+  end
 
   def create
     @color = "normal"
