a00ae60c691c659c129ab913e0f9e3a95777911e
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-03-11 12:18:04 +0900

    admin機能コントローラー作成

---
+++ app/controllers/admin/user_sessions_controller.rb
@@ -0,0 +1,2 @@
+class Admin::UserSessionsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'dashboards/index'
+  end
   root 'tops#top'
 
   get 'terms', to: 'tops#terms'
