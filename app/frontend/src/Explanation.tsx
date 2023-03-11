874ada46431539d478fca69be53993d557776b8a
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-03-11 19:15:00 +0900

    テストタイプのTOP修正

--- app/frontend/src/Explanation.tsx
+++ app/frontend/src/Explanation.tsx
@@ -76,7 +76,10 @@ const Explanation = () => {
         トグルを利用することで、画面上に表示したいものを自由に選択できます。
         </Fade>
       </div>
-      <div className="col-span-2 mt-10 mx-2 drop-shadow-2xl">
+      <div className="col-span-2 sm:col-span-4 border-b border-neutral-700 text-neutral text-2xl xl:text-5xl mt-10 mx-2 drop-shadow-2xl">
+        <a href="https://midpoint.magia.runteq.jp/users/new" rel="noopener noreferrer">
+        アカウント登録はこの文章をクリック
+        </a>
       </div>
       <div className="col-span-2 mt-10 mx-2 drop-shadow-2xl">
       </div>
