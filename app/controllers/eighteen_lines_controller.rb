334fc00fe5bfc50d258ee55b05034c5f23d90bf3
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-03-13 13:57:30 +0900

    プロットポイントの分離

--- app/controllers/eighteen_lines_controller.rb
+++ app/controllers/eighteen_lines_controller.rb
@@ -2,40 +2,45 @@ class EighteenLinesController < ApplicationController
   def index
     @current_page = "tab2"
     @plot = Plot.find(params[:plot_id])
-    @eighteen_lines = @plot.eighteen_lines.order(created_at: :asc)
+    @eighteen_lines = @plot.eighteen_lines.order(number: :asc)
     @eighteen_line = EighteenLine.new
 
     redirect_to root_path unless @plot.user == current_user
   end
 
+  def new
+    @eighteen_line = EighteenLine.new
+  end
+
   def create
-    eighteen_line = EighteenLine.new(eighteen_line_params)
+    @plot = Plot.find(params[:plot_id])
+    @eighteen_line = EighteenLine.new(eighteen_line_params)
     if eighteen_line.save
-      redirect_to plot_path(eighteen_line.plot), success: "作成しました！"
+      redirect_to plot_eighteen_lines_path(@eighteen_line.plot), success: "作成しました！"
     else
-      redirect_to plot_path(eighteen_line.plot), danger: "作成できませんでした"
+      redirect_to plot_eighteen_lines_path(@eighteen_line.plot), danger: "作成できませんでした"
     end
   end
 
   def update
-    eighteen_line = EighteenLine.find(params[:id])
-    if eighteen_line.update(eighteen_line_params.merge(plot_id: eighteen_line.plot_id))
-      redirect_to plot_path(eighteen_line.plot), notice: '更新しました'
+    @eighteen_line = EighteenLine.find(params[:id])
+    if @eighteen_line.update(eighteen_line_params.merge(plot_id: @eighteen_line.plot_id))
+      redirect_to plot_eighteen_lines_path(@eighteen_line.plot), notice: '更新しました'
     else
-      redirect_to plot_path(eighteen_line.plot), success: "更新できませんでした。"
+      redirect_to plot_eighteen_lines_path(@eighteen_line.plot), success: "更新できませんでした。"
     end
   end
 
   def destroy
-    eighteen_line = EighteenLine.find(params[:id])
-    eighteen_line.destroy!
-    redirect_to plot_path(eighteen_line.plot), status: :see_other, success: "削除しました！"
+    @eighteen_line = EighteenLine.find(params[:id])
+    @eighteen_line.destroy!
+    redirect_to plot_eighteen_lines_path(@eighteen_line.plot), status: :see_other, success: "削除しました！"
   end
 
   private
 
   def eighteen_line_params
-    params.require(:eighteen_line).permit(:body).merge(plot_id: params[:plot_id])
+    params.require(:eighteen_line).permit(:number, :body, :explanation, :name).merge(plot_id: params[:plot_id])
   end
   
 end
