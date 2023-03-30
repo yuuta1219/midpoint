5d6fda7ec384a57612422e6c5fcb9afeb7c05775
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-03-31 06:31:52 +0900

    rails db:rollback

--- db/migrate/20230330212639_add_tutorial_completed_to_users.rb
+++ db/migrate/20230330212639_add_tutorial_completed_to_users.rb
@@ -1,5 +1,7 @@
 class AddTutorialCompletedToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :tutorial_completed, :boolean
+    
+    User.where(tutorial_completed: nil).update_all(tutorial_completed: false)
   end
 end
