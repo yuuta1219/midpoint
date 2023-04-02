92c5e1fc55d868659adf04bac6121d9d6013341b
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-04-02 19:13:59 +0900

    メールアドレスの変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,4 +73,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = Settings.default_url_options.to_h
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "midplot.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_APP_PASSWORD"]
+  }
 end
