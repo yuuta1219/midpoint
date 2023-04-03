6b426781447082f46c0a6945d30299870ea34bcd
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-04-03 17:27:37 +0900

    aws3sの設定

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,20 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['S3_ACCESS_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET_KEY'],
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory  = 'midplot'
+  else
+    config.storage :file
+    config.enable_processing = Rails.env.development?
+  end
+end
