cd298f3052f93fbdb96066b7175c184419b83d3c
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-05-22 11:39:51 +0900

    notescontrolle初期設定

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,47 @@
 class NotesController < ApplicationController
+  def index
+    @notes = Note.all
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+    if @note.save
+      redirect_to @note
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    if @note.update(note_params)
+      redirect_to @note
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @note = Note.find(params[:id])
+    @note.destroy
+    redirect_to notes_path
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :content)
+  end
 end
